2117756cdc2c7b3c850499c32768b082d3ae4835
brandonfox <brandonfox@hotmail.co.uk>
2022-05-05 19:56:22 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pull-and-filter';
+export class AppComponent implements OnInit{
+  filter: string = '';
+  data: string[] = [];
+  filteredCategories: {index: number, category: string}[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get('https://api.publicapis.org/categories').pipe(
+      map((resp: {categories?: string[]}) => {
+        return resp.categories!!;
+      })
+    ).subscribe(categories => {
+      this.data = categories;
+      this.filterCategories();
+    })
+  }
+
+  filterCategories() {
+    // Super simple filter (no sorting)
+    const categories = this.data.map((value, index) => {
+      return { index: index, category: value };
+    })
+    if (this.filter.length == 0) this.filteredCategories = categories;
+    else this.filteredCategories = categories.filter((value) => value.category.toLowerCase().includes(this.filter.toLowerCase()))
+  }
+
 }
